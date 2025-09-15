feff0c33fc2ba761604af8463e4e18e26cd74c99
Alex Coronell <alexcoronell@gmail.com>
2025-09-15 10:50:14 -0500

    Empanadas data added in Spanish and English

--- src/core/data/index.data.ts
+++ src/core/data/index.data.ts
@@ -2,10 +2,14 @@ import { businessDataEnglish } from "./business.data.en";
 import { businessDataSpanish } from "./business.data.es";
 import { menuDataEnglish } from "./menu.data.en";
 import { menuDataSpanish } from "./menu.data.es";
+import { empanadaEnglish } from "./products.data.en";
+import { empanadaSpanish } from "./products.data.es";
 
 export {
   businessDataEnglish,
   businessDataSpanish,
   menuDataEnglish,
   menuDataSpanish,
+  empanadaEnglish,
+  empanadaSpanish,
 };

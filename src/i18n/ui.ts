d2827a0df593c75607320218228c96bbf903d31f
Alex Coronell <alexcoronell@gmail.com>
2025-11-14 13:46:39 -0500

    i18n/ui.ts file completed

---
+++ src/i18n/ui.ts
@@ -0,0 +1,29 @@
+export const languages = {
+  es: 'es Español',
+  en: 'en English',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+  es: {
+    'nav.home': 'Inicio',
+    'nav.products': 'Productos',
+    'nav.contact': 'Contacto',
+    copyRight: 'Todos los derechos reservados',
+    'message.facebook': 'Visita y siguenos en nuestra pagina de Facebook',
+    'message.instagram': 'Visita y siguenos en nuestra pagina de Instagram',
+    'message.tiktok': 'Visita y siguenos en nuestra pagina de Tiktok',
+    'message.whatsapp': 'Visita y siguenos en nuestra pagina de WhatsApp',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.products': 'Products',
+    'nav.contact': 'Contact',
+    copyRight: 'All rights reserved',
+    'message.facebook': 'Visit and follow us on our Facebook page',
+    'message.instagram': 'Visit and follow us on our Instagram page',
+    'message.tiktok': 'Visit and follow us on our Tiktok page',
+    'message.whatsapp': 'Visit and follow us on our WhatsApp page',
+  },
+};
